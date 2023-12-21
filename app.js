94ac46745e307f7c70c8da5cf9a5ed66b64d1e63
Artem Peshkov <a.o.peshkov@yandex.ru>
2023-12-21 22:12:00 +0500

    hard

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log(1)
+// const dotenv = require('dotenv')
+// dotenv.config()
+// console.log(process.env.PORT)
+// console.log(process.env.NODE_ENV)
+// console.log(process.argv)
+// console.log(path.join(__dirname,'..'))
+
+// console.log(path.resolve('first','second','third'))
+
+// const fullPath = path.resolve('first','second','third.js')
+
+// console.log(path.parse(fullPath))
+
+// const path = require('path')
+
+// const siteURL = 'http://localhost:8000/users?id=5123'
+
+// const url = new URL(siteURL)
+
+// console.log(url)
+
+// fs.mkdir(path.join(__dirname,'dir'),(err)=>{
+
+//   if(err){
+//     console.log(err)
+//     return
+//   }
+//   console.log('create')
+
+// })
+
+// fs.rmdir(path.join(__dirname,'dir'),(err)=>{
+//   if(err){
+//     console.log(err)
+//     return
+//   }
+//   console.log('delete')
+// })
+
+// fs.writeFile(path.join(__dirname,'text.txt'), 'test',(err)=>{
+//     if(err){
+//       console.log(err)
+//       return
+//     }
+//     console.log('create')
+//   })
+
+const path = require('path');
+const fs = require('fs');
+const os = require('os')
+
